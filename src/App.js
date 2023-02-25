0a50381686e781952f61700d8f0ab50d47104dab
Daniel <danielvillery@gmail.com>
2023-02-25 15:42:56 -0500

    made group page with members route to member profile

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import "./App.css";
 import NewEvent from "./components/NewEvent";
 import NewActivity from "./components/NewActivity";
 import VotePage from "./components/VotePage";
-import GroupPage from "./components/Groups";
+import { GroupPage, Group } from "./components/Groups";
 import { useState } from "react";
 import { Route, Routes, Link, UseParams, useNavigate } from "react-router-dom";
 import PostVoteEvent from "./components/PostVote";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/" element={<NewEvent />}></Route>
-        <Route path="/new/activity" element={<NewActivity />}></Route>
-        <Route path="/event/:eventId" element={<PostVoteEvent />}></Route>
-        <Route path="/group" element={<GroupPage />}></Route>
-        <Route path="/group/:groupId" element={<NewActivity />}></Route>
-        <Route path="/voting" element={<VotePage />}></Route>
+        <Route path='/' element={<NewEvent />}></Route>
+        <Route path='/new/activity' element={<NewActivity />}></Route>
+        <Route path='/event/:eventId' element={<PostVoteEvent />}></Route>
+        <Route path='/group' element={<GroupPage />}></Route>
+        <Route path='/group/:groupId' element={<Group />}></Route>
+        <Route path='/voting' element={<VotePage />}></Route>
+        <Route path='/profile/:username' element={<VotePage />}></Route>
       </Routes>
     </div>
   );
