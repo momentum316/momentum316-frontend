862eb8319a38d07bf7d24f0319abcb8f5e1fdbad
Daniel <danielvillery@gmail.com>
2023-02-25 15:03:10 -0500

    added user group page and mapped all groups out on page

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -1,55 +1,48 @@
 import { Avatar, AvatarGroup, Grid, Button, ButtonGroup } from "@mui/material";
 import { Route, Routes, Link, UseParams, useNavigate } from "react-router-dom";
 import FooterObject from "./Footer";
+import axios from "axios";
+
+import { useState, useEffect } from "react";
 
 export default function GroupPage() {
+  const [groups, setGroups] = useState(null);
+  useEffect(() => {
+    axios
+      // need to change this to dynamic username once login page is ready
+      .get("https://congregate.onrender.com/villeryd/groups")
+      .then((response) => setGroups(response.data));
+  }, []);
+
   const navigate = useNavigate();
   return (
-    <div>
-      <p>Groups Page</p>
+    groups && (
+      <div>
+        <p>Groups Page</p>
 
-      <br />
-      <div className="group-grid">
-        <Grid
-          container
-          direction="columns"
-          spacing={3}
-          columns={{ xs: 12, sm: 8, md: 12 }}
-        >
-          <Grid item xs={4} sm={4} md={4}>
-            <Avatar
-              onClick={() => navigate("/")}
-              alt="Remy Sharp"
-              src="/static/images/avatar/1.jpg"
-            />
+        <br />
+        <div className='group-grid'>
+          <Grid
+            container
+            direction='columns'
+            spacing={3}
+            columns={{ xs: 12, sm: 8, md: 12 }}
+          >
+            {groups.map((g) => (
+              <Grid item xs={4} sm={4} md={4}>
+                <Avatar
+                  key={g.id}
+                  onClick={() => navigate(`/group/${g.id}`)}
+                  alt={g.title}
+                  src='/static/images/avatar/1.jpg'
+                />
+                <p>{g.title}</p>
+              </Grid>
+            ))}
           </Grid>
-          <Grid item xs={4} sm={4} md={4}>
-            <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-          </Grid>
-          <Grid item xs={4} sm={4} md={4}>
-            <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-          </Grid>
-          <Grid item xs={4} sm={4} md={4}>
-            <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-          </Grid>
-          <Grid item xs={4} sm={4} md={4}>
-            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-          </Grid>
-          <Grid item xs={4} sm={4} md={4}>
-            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-          </Grid>
-          <Grid item xs={4} sm={4} md={4}>
-            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-          </Grid>
-          <Grid item xs={4} sm={4} md={4}>
-            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-          </Grid>
-          <Grid item xs={4} sm={4} md={4}>
-            <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-          </Grid>
-        </Grid>
+        </div>
+        <FooterObject />
       </div>
-      <FooterObject />
-    </div>
+    )
   );
 }
