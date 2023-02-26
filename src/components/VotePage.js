53e08337d7a74b05f13f4fd4a477efc0f59be10f
CapelHoworth <capelhoworth@gmail.com>
2023-02-26 15:58:52 -0500

    vert list positioned on vote page

--- src/components/VotePage.js
+++ src/components/VotePage.js
@@ -1,11 +1,18 @@
 import { VoterSlide, EventSlide } from "./Slides";
 import { FooterObject, VertList } from "./Footer";
+import { Grid } from "@mui/material";
 
 export default function VotePage() {
   return (
     <>
-      <EventSlide />
-      <VertList />
+      <Grid container spacing={2}>
+        <Grid item xs={10} md={8}>
+          <EventSlide />
+        </Grid>
+        <Grid item xs={2} md={4}>
+          <VertList />
+        </Grid>
+      </Grid>
       <br />
       <VoterSlide />
       <FooterObject />
