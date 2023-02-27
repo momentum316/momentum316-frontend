77f0d1ca73286d840bc6b4e8a51c15481a17f7ae
Daniel <danielvillery@gmail.com>
2023-02-27 14:42:29 -0500

    google login passing credential, need to pass to backend

--- src/App.js
+++ src/App.js
@@ -7,23 +7,28 @@ import { GroupPage, Group } from "./components/Groups";
 import { useState } from "react";
 import { Route, Routes, Link, UseParams, useNavigate } from "react-router-dom";
 import PostVoteEvent from "./components/PostVote";
+import Login from "./components/LoginPage";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Routes>
-        <Route path="/" element={<NewEvent />}></Route>
-        <Route path="/new/activity" element={<NewActivity />}></Route>
-        <Route path="/event/:eventId" element={<PostVoteEvent />}></Route>
-        <Route path="/group" element={<GroupPage />}></Route>
-        <Route path="/group/:groupId" element={<Group />}></Route>
-        <Route path="/voting" element={<VotePage />}></Route>
-        <Route path="/profile/:username" element={<VotePage />}></Route>
-        <Route path="/group/:groupId" element={<Group />}></Route>
-        <Route path="/group/:groupId/vote" element={<VotePage />}></Route>
-        <Route path="/group/:groupId/discussion" element={<Group />}></Route>
-        <Route path="/group/:groupId/events" element={<Group />}></Route>
-        <Route path="/group/:groupId/vote/:eventId" element={<Vote />}></Route>
+        <Route path='/' element={<NewEvent />}></Route>
+        <Route path='/new/activity' element={<NewActivity />}></Route>
+        <Route
+          path='/event/:groupId/:eventId'
+          element={<PostVoteEvent />}
+        ></Route>
+        <Route path='/group' element={<GroupPage />}></Route>
+        <Route path='/group/:groupId' element={<Group />}></Route>
+        <Route path='/voting' element={<VotePage />}></Route>
+        <Route path='/profile/:username' element={<VotePage />}></Route>
+        <Route path='/group/:groupId' element={<Group />}></Route>
+        <Route path='/group/:groupId/vote' element={<VotePage />}></Route>
+        <Route path='/group/:groupId/discussion' element={<Group />}></Route>
+        <Route path='/group/:groupId/events' element={<Group />}></Route>
+        <Route path='/group/:groupId/vote/:eventId' element={<Vote />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
     </div>
   );
