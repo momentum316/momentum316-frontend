24253ec82523fba195cae40b819b2a9cded1d0fc
CapelHoworth <capelhoworth@gmail.com>
2023-02-26 20:23:08 -0500

    moved voteslide to votepage, changed vote arrows to icons

--- src/components/VotePage.js
+++ src/components/VotePage.js
@@ -1,6 +1,8 @@
-import { VoterSlide, EventSlide } from "./Slides";
+import { EventSlide } from "./Slides";
 import { FooterObject, VertList } from "./Footer";
-import { Grid } from "@mui/material";
+import { Grid, Box, IconButton, Divider, Container } from "@mui/material";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export default function VotePage() {
   return (
@@ -14,7 +16,43 @@ export default function VotePage() {
         </Grid>
       </Grid>
       <br />
-      <VoterSlide />
+      // Start VotingSlide
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Box
+            sx={{
+              height: 70,
+              backgroundColor: "primary.dark",
+              "&:hover": {
+                backgroundColor: "primary.main",
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+          >
+            <IconButton onClick={() => console.log("up")}>
+              <KeyboardArrowUpIcon />
+            </IconButton>
+            <Divider />
+            <IconButton onClick={() => console.log("down")}>
+              <KeyboardArrowDownIcon sx={{ fontWeight: "bold" }} />
+            </IconButton>
+          </Box>
+        </Grid>
+        <Grid item xs={9}>
+          <Box
+            sx={{
+              height: 70,
+              backgroundColor: "primary.dark",
+              "&:hover": {
+                backgroundColor: "primary.main",
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+          >
+            <Container>Some Other Dope Shit</Container>
+          </Box>
+        </Grid>
+      </Grid>
       <FooterObject />
     </>
   );
