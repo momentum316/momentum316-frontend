ee704269bc65e473628da674391d10c7b491f896
Daniel <danielvillery@gmail.com>
2023-03-06 19:13:11 -0500

    fixed scroll lock bug on homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,13 +11,18 @@ import { useNavigate, useParams } from "react-router-dom";
 export function FooterObject({ user }) {
   const navigate = useNavigate();
   return (
-    <div className="footer-wrapper" style={{ position: "fixed", bottom: 0 }}>
-      <footer className="footer-wrapper-content">
+    <div
+      className='footer-wrapper'
+      // style={{ position: "fixed", bottom: 0 }}
+    >
+      <footer
+      // className="footer-wrapper-content"
+      >
         <ButtonGroup
           fullWidth
-          size="large"
-          color="secondary"
-          variant="contained"
+          size='large'
+          color='secondary'
+          variant='contained'
         >
           <Button onClick={() => navigate(`/home/${user.user.username}`)}>
             Home
