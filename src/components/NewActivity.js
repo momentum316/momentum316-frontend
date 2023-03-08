0e0312dc3fb8b9c080c9072b41f73404b88fa01e
Daniel <danielvillery@gmail.com>
2023-03-08 10:33:12 -0500

    still counts as EOD

--- src/components/NewActivity.js
+++ src/components/NewActivity.js
@@ -63,15 +63,15 @@ export function NewActivity({
     <div>
       <Card elevation={0}>
         <CardHeader
-          variant="h5"
-          align="center"
-          title="Add Activity to Event"
+          variant='h5'
+          align='center'
+          title='Add Activity to Event'
         ></CardHeader>
       </Card>
       <br />
       <TextField
-        id="activity-title"
-        label="Activity Title"
+        id='activity-title'
+        label='Activity Title'
         fullWidth
         value={activityTitle}
         onChange={(e) => {
@@ -83,7 +83,7 @@ export function NewActivity({
         <br />
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <TimePicker
-            label="Start Time"
+            label='Start Time'
             value={startValue}
             onChange={handleStart}
             renderInput={(params) => <TextField {...params} />}
@@ -92,7 +92,7 @@ export function NewActivity({
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <br />
           <TimePicker
-            label="End Time"
+            label='End Time'
             value={endValue}
             onChange={handleEnd}
             renderInput={(params) => <TextField {...params} />}
@@ -104,7 +104,7 @@ export function NewActivity({
       <Stack spacing={4}>
         <TextField
           fullWidth
-          label="Location"
+          label='Location'
           value={location}
           onChange={(e) => {
             setLocation(e.target.value);
@@ -112,8 +112,8 @@ export function NewActivity({
           }}
         />
         <TextField
-          id="description-box"
-          label="Description"
+          id='description-box'
+          label='Description'
           multiline
           rows={4}
           fullWidth
@@ -127,7 +127,7 @@ export function NewActivity({
       {/* CANCEL BUTTON (clear fields or back to event?)*/}
       <br />
       <Stack>
-        <Button onClick={() => navigate("/")} fullWidth variant="contained">
+        <Button onClick={() => navigate("/")} fullWidth variant='contained'>
           Cancel
         </Button>
       </Stack>
@@ -137,5 +137,119 @@ export function NewActivity({
 
 export function AddActivity({ user }) {
   const { groupId, eventId } = useParams();
-  return <NewActivity user={user} groupId={groupId} eventId={eventId} />;
+  const navigate = useNavigate();
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [startValue, setStartValue] = useState(null);
+  const [endValue, setEndValue] = useState(null);
+  const [activityTitle, setActivityTitle] = useState("");
+  const handleStart = (newValue) => {
+    var d = new Date(newValue);
+    var date = d.toISOString();
+    setStartValue(date);
+  };
+  const handleEnd = (newValue) => {
+    var d = new Date(newValue);
+    var date = d.toISOString();
+    setEndValue(date);
+  };
+
+  const handleAdd = (e) => {
+    axios
+      .post(
+        `${process.env.REACT_APP_BACKEND_URL}/new/activity/`,
+        {
+          title: activityTitle,
+          event_id: eventId,
+          description: description,
+          location: location,
+          start_time: startValue,
+          end_time: endValue,
+        },
+        {
+          headers: {
+            Authorization: `token ${user.token}`,
+          },
+        }
+      )
+      .then((res) => navigate(`/group/${groupId}/vote/${eventId}`));
+  };
+
+  return (
+    <div>
+      <Card elevation={0}>
+        <CardHeader
+          variant='h5'
+          align='center'
+          title='Add Activity to Event'
+        ></CardHeader>
+      </Card>
+      <br />
+      <TextField
+        id='activity-title'
+        label='Activity Title'
+        fullWidth
+        value={activityTitle}
+        onChange={(e) => {
+          setActivityTitle(e.target.value);
+        }}
+      ></TextField>
+      <Stack>
+        <br />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <TimePicker
+            label='Start Time'
+            value={startValue}
+            onChange={handleStart}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <br />
+          <TimePicker
+            label='End Time'
+            value={endValue}
+            onChange={handleEnd}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+        <br />
+      </Stack>
+      <br />
+      <Stack spacing={4}>
+        <TextField
+          fullWidth
+          label='Location'
+          value={location}
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
+        />
+        <TextField
+          id='description-box'
+          label='Description'
+          multiline
+          rows={4}
+          fullWidth
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
+      </Stack>
+      {/* CANCEL BUTTON (clear fields or back to event?)*/}
+      <br />
+      <Stack spacing={3}>
+        <Button onClick={() => navigate(-1)} fullWidth variant='contained'>
+          Cancel
+        </Button>
+      </Stack>
+      <br />
+      <Stack>
+        <Button onClick={(e) => handleAdd(e)} fullWidth variant='contained'>
+          Add Activity
+        </Button>
+      </Stack>
+    </div>
+  );
 }
--- src/components/VotePage.js
+++ src/components/VotePage.js
@@ -52,20 +52,20 @@ export function VotePage({ user }) {
     activeVote && (
       <>
         {/* <ActivitySlide user={user} /> */}
-        <Box alignItems="center">
-          <Grid container alignItems="center" justifyContent="right">
+        <Box alignItems='center'>
+          <Grid container alignItems='center' justifyContent='right'>
             <Grid item xs={2}>
               <IconLogo />
             </Grid>
             <Grid item xs={8}>
               <Card elevation={0}>
-                <CardHeader title={`${group}`} subheader="Open Vote Events" />
+                <CardHeader title={`${group}`} subheader='Open Vote Events' />
               </Card>
             </Grid>
             <Grid item xs={2}>
-              <Tooltip title="More Options">
+              <Tooltip title='More Options'>
                 <IconButton>
-                  <MoreVertIcon fontSize="large" />
+                  <MoreVertIcon fontSize='large' />
                 </IconButton>
               </Tooltip>
             </Grid>
@@ -89,7 +89,7 @@ export function VotePage({ user }) {
             ))
           ) : (
             <Grid item>
-              <Typography variant="h5">No Active Votes</Typography>
+              <Typography variant='h5'>No Active Votes</Typography>
             </Grid>
           )}
         </Grid>
@@ -126,7 +126,7 @@ export function Vote({ user }) {
         }
       )
       .then((res) => {
-        navigate(`/group/${groupId}/vote/${eventId}`);
+        setEventVoter(true);
       });
   };
 
@@ -163,7 +163,7 @@ export function Vote({ user }) {
         >
           <ActivitySlide event={eventParent} />
         </Box>
-        <Grid container spacing={2} justifyContent="center" alignItems="center">
+        <Grid container spacing={2} justifyContent='center' alignItems='center'>
           <Grid item xs={12}>
             <GroupTabs />
           </Grid>
@@ -190,7 +190,7 @@ export function Vote({ user }) {
             {!eventVoter && (
               <Button
                 fullWidth
-                variant="contained"
+                variant='contained'
                 onClick={(e) => handleEvent(e)}
               >
                 Add an Activity
@@ -201,10 +201,10 @@ export function Vote({ user }) {
             {!eventVoter && (
               <Button
                 fullWidth
-                variant="contained"
+                variant='contained'
                 onClick={(e) => handleVoteSubmit(e)}
               >
-                Submit Your Votes
+                Lock Your Votes
               </Button>
             )}
           </Grid>
