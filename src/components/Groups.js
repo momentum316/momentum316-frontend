f216056c0c527c2d4f3517a541ce60a253d18407
jeromecox <jeromecox@hotmail.com>
2023-03-07 20:25:46 -0600

    Add event button on group event list page works

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -18,17 +18,17 @@ import {
   Input,
   Tooltip,
   Typography,
-} from "@mui/material";
-import { Route, Routes, Link, useParams, useNavigate } from "react-router-dom";
-import ContentPasteIcon from "@mui/icons-material/ContentPaste";
-import { GroupTabs, SmallLogo, ActivityCard } from "./NoteCards";
-import { GroupsHeader, EventsHeader } from "./Headers";
-import axios from "axios";
-import backend_url from "../render.json";
+} from '@mui/material';
+import { Route, Routes, Link, useParams, useNavigate } from 'react-router-dom';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import { GroupTabs, SmallLogo, ActivityCard } from './NoteCards';
+import { GroupsHeader, GroupEventsHeader } from './Headers';
+import axios from 'axios';
+import backend_url from '../render.json';
 
-import { useState, useEffect } from "react";
-import { CopyAll } from "@mui/icons-material";
-import { GroupMembersHeader } from "./Headers";
+import { useState, useEffect } from 'react';
+import { CopyAll } from '@mui/icons-material';
+import { GroupMembersHeader } from './Headers';
 
 // LIST OF USER'S GROUPS
 export function GroupPage({ user }) {
@@ -144,7 +144,7 @@ export function Group({ user }) {
 
 // CREATE NEW GROUP OR COPY LINK TO GROUP INVITE PAGE
 export function NewGroup({ user }) {
-  let NewGroup = "create";
+  let NewGroup = 'create';
   const [groups, setGroups] = useState(NewGroup);
   const [choices, setChoices] = useState(null);
   const [groupName, setGroupName] = useState(null);
@@ -182,7 +182,7 @@ export function NewGroup({ user }) {
       )
       .then((res) => {
         console.log(res);
-        navigate("/group");
+        navigate('/group');
       });
   };
 
@@ -314,7 +314,7 @@ export function GroupEvents({ user }) {
     events && (
       <Grid container spacing={2} alignItems="center" justifyContent="left">
         <Grid item xs={12}>
-          <EventsHeader user={user} />
+          <GroupEventsHeader user={user} />
         </Grid>
         <Grid item xs={12}>
           <GroupTabs />
@@ -369,7 +369,7 @@ export function LeaveGroup({ user }) {
       )
       .then((res) => {
         console.log(res);
-        navigate("/group");
+        navigate('/group');
       });
   };
   return (
